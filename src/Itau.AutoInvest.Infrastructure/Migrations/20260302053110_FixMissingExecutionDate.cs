3ae1e76b45652c18fb4606c2f6e1aa19307f9156
Gabriel Araujo <gabriel.araujo2902@outlook.com>
2026-03-02 02:31:48 -0300

    chore: fix missing execution date migration

---
+++ src/Itau.AutoInvest.Infrastructure/Migrations/20260302053110_FixMissingExecutionDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Itau.AutoInvest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMissingExecutionDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataExecucao",
+                table: "OrdensCompra",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataExecucao",
+                table: "OrdensCompra");
+        }
+    }
+}
